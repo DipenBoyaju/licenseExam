eb74a501e8e2a6af794c304df09f251029c07aa8
Dipen <dipendraboyaju222@gmail.com>
2025-07-09 17:59:15 +0545

    change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className=" border-t border-zinc-900/10 py-6">
       <p className="font-inter text-center text-sm text-zinc-800">
-        © 2025 <a href="https://dipenboyaju.com.np/" className="underline cursor-pointer hover:text-orange-400 text-orange-500 transition" target="_blank">Dipen Boyaju</a>. All rights reserved.
+        © 2025 <a href="https://dipenboyaju.com.np/" className="underline cursor-pointer hover:text-orange-400 text-orange-500 transition" target="_blank">Dipen Boyaju</a> & <a href="https://www.linkedin.com/in/ashish-lawaju-aba628238/?trk=public_profile_samename-profile&originalSubdomain=np" className="underline cursor-pointer hover:text-orange-400 text-orange-500 transition" target="_blank">Ashish Lawaju</a>. All rights reserved.
       </p>
 
     </div>
