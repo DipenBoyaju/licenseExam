828887b063b75ba2f1be11ccfc95c6f90a30f257
Dipen <dipendraboyaju222@gmail.com>
2025-08-14 19:41:33 +0545

    change

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.SITE_URL || "https://neclicenseexam.vercel.app",
-  generateRobotsTxt: true,
-  sitemapSize: 50000, // max number of URLs in one sitemap file (Google limit is 50,000)
-  outDir: "./public", // where sitemap files will be generated
-};
+// /** @type {import('next-sitemap').IConfig} */
+// module.exports = {
+//   siteUrl: process.env.SITE_URL || "https://neclicenseexam.vercel.app",
+//   generateRobotsTxt: true,
+//   sitemapSize: 50000, // max number of URLs in one sitemap file (Google limit is 50,000)
+//   outDir: "./public", // where sitemap files will be generated
+// };
---
+++ src/sitemap.ts
@@ -0,0 +1,47 @@
+import { MetadataRoute } from "next";
+import { blogData } from "./app/constant/constant";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const siteUrl = process.env.SITE_URL || "https://neclicenseexam.vercel.app";
+
+  const blogEntries: MetadataRoute.Sitemap = blogData.map((blog) => ({
+    url: `${siteUrl}/blog/${blog.id}`,
+    lastModified: new Date(),
+  }));
+
+  return [
+    {
+      url: `${siteUrl}`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/take-test`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/take-test/computer-engineering`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/take-test/computer-engineering/quiz`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/faculty/computer-engineering`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/faculty/civil-engineering`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/review`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteUrl}/blog`,
+      lastModified: new Date(),
+    },
+    ...blogEntries,
+  ];
+}
